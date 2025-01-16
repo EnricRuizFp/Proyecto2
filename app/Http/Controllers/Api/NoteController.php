226c8499902a71c3703d779465f6f98565931981
Daniel Lobera <danielloberafp@ibf.cat>
2025-01-16 17:25:53 +0100

    Add NoteController routes to API

---
+++ app/Http/Controllers/Api/NoteController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Models\Note;
+
+class NoteController extends Controller
+{
+    public function index (): JsonResponse
+    {
+        $notes = Note::all();
+        return response()->json($notes, 200);
+    }
+
+    public function store (Request $request): JsonResponse
+    {
+        $note = Note::create($request->all());
+        return response()->json([
+            'success'=>true,
+            'data' => $note
+        ], 201);
+    }
+
+    public function show ($id): JsonResponse
+    {
+        $note = Note::find($id);
+        return response()->json($note, 200);
+    }
+
+    public function update (Request $request, $id): JsonResponse
+    {
+        $note = Note::find($id);
+        $note -> update($request->all());
+        return response()->json($note, 200);
+
+    }
+
+    public function destroy ($id): JsonResponse
+    {
+        $note = Note::find($id);
+        $note -> delete();
+        return response()->json([
+            'success'=>true
+        ], 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\NoteController;
 
 Route::post('forget-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('forget.password.post');
 Route::post('reset-password', [ResetPasswordController::class, 'reset'])->name('password.reset');
@@ -51,3 +52,10 @@ Route::get('category-list', [CategoryController::class, 'getList']);
 Route::get('get-posts', [PostControllerAdvance::class, 'getPosts']);
 Route::get('get-category-posts/{id}', [PostControllerAdvance::class, 'getCategoryByPosts']);
 Route::get('get-post/{id}', [PostControllerAdvance::class, 'getPost']);
+
+
+Route::get('notes', [NoteController::class, 'index'])->name('notes.index');
+Route::post('notes',[NoteController::class,'store'])->name('notes.store');
+Route::get('notes/{id}',[NoteController:: class, 'show'])->name(name: 'notes.show');
+Route::put('notes/{id}',[NoteController:: class, 'update'])->name(name: 'notes.update');
+Route::delete('notes/{id}',[NoteController:: class, 'destroy'])->name(name: 'notes.destroy');
