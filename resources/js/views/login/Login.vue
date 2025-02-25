fd1a0383c4ed8bcb3f4ab8561b245891ab49f693
EnricRuizFp <EnricRuizFp>
2025-02-25 15:52:20 +0100

    Login page done.

--- resources/js/views/login/Login.vue
+++ resources/js/views/login/Login.vue
@@ -3,10 +3,17 @@
         <div>
 
             <form @submit.prevent="submitLogin" id="loginForm">
+
+                <!-- LOGIN title -->
+                <div id="loginFormTitle">
+                    <h2 class="h2 white-color">INICIAR SESIÓN</h2>
+                </div>
+
                 <div id="loginFormContent">
+
                     <!-- Email -->
                     <div id="emailField">
-                        <label for="email" class="loginFormLabels">{{ $t('email') }}</label>
+                        <label for="email" class="loginFormLabels p4">{{ $t('email') }}</label>
                         <input v-model="loginForm.email" id="email" class="loginFormFields" type="email" required autofocus autocomplete="username">
                         <!-- Validation Errors -->
                         <div>
@@ -17,7 +24,7 @@
                     </div>
                     <!-- Password -->
                     <div id="passwordField">
-                        <label for="password"  class="loginFormLabels">
+                        <label for="password"  class="loginFormLabels p4">
                             {{ $t('password') }}
                         </label>
                         <input v-model="loginForm.password" id="password" class="loginFormFields" type="password" required autocomplete="current-password">
@@ -29,21 +36,31 @@
                         </div>
                     </div>
                     <!-- Remember me -->
-                    <div id="rememberMeField">
+                    <div id="rememberMeField" class="color-white p5">
                         <input type="checkbox" name="remember" v-model="loginForm.remember" id="flexCheckIndeterminate">
                         <label for="flexCheckIndeterminate">
                             {{ $t('remember_me') }}
                         </label>
                     </div>
 
+                    <!-- Ya tienes cuenta? -->
+                    <div id="contenedorNoTienesCuenta">
+                        <p class="p5 white-color">¿Todavía no tienes cuenta? <a href="/register" class="linkText underline">Regístrate</a></p>
+
+                    </div>
+
                     <!-- Buttons -->
-                    <div id="loginButton">
-                        <button :class="{ 'opacity-25': processing }" :disabled="processing">
+                    <div>
+                        <button :class="{ 'opacity-25': processing }" :disabled="processing" id="loginButton" class="primary-button">
                             {{ $t('login') }}
                         </button>
                     </div>
                 </div>
-                <router-link :to="{name: 'auth.forgot-password'}">{{ $t('forgot_password')}}</router-link>
+
+                <div id="forgotPasswordText">
+                    <router-link :to="{name: 'auth.forgot-password'}" class="white-color">{{ $t('forgot_password')}}</router-link>
+                </div>
+                
             </form>
         </div>
     </div>
