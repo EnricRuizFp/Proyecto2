ddeb8d32c1f6758074e5a0f1ba96e587492034de
EnricRuizFp <EnricRuizFp>
2025-03-10 17:40:01 +0100

    Added coordinate column to game_players table

--- database/migrations/2025_02_06_161857_2023_01_01_000000_create_game_players_table.php.php
+++ database/migrations/2025_02_06_161857_2023_01_01_000000_create_game_players_table.php.php
@@ -14,6 +14,7 @@ return new class extends Migration
             $table->unsignedBigInteger('game_id');
             $table->unsignedBigInteger('user_id');
             $table->date('joined')->default(DB::raw('(CURDATE())'));
+            $table->json('coordinates');
 
             $table->foreign('game_id')->references('id')->on('games');
             $table->foreign('user_id')->references('id')->on('users');
