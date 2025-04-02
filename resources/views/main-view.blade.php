14972abde26ba5406a86b6e50a708fe1b426ac8a
Daniel Lobera <danielloberafp@ibf.cat>
2025-04-02 19:04:16 +0200

    Add favicon and update main view layout; clean up whitespace in main-view.blade.php

--- resources/views/main-view.blade.php
+++ resources/views/main-view.blade.php
@@ -1,15 +1,19 @@
 @php
-    $config = [
-        'appName' => config('app.name'),
-        'locale' => $locale = app()->getLocale(),
-        'locales' => config('app.locales'),
-    ];
+$config = [
+'appName' => config('app.name'),
+'locale' => $locale = app()->getLocale(),
+'locales' => config('app.locales'),
+];
 @endphp
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="light">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="icon" type="image/svg+xml" href="{{ asset('images/anchor.svg') }}">
+    <link rel="alternate icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>DE BATTLESHIP</title>
@@ -26,11 +30,13 @@
         window.config = @json($config);
     </script>
     <!-- Scripts -->
-{{--    @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
 </head>
+
 <body class="font-sans antialiased" id="app">
     <router-view></router-view>
 </body>
+
 </html>
